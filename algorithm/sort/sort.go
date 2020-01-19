694f487b049147b0ae6fbcdc0401869b8470c447
hejun <13237124836@163.com>
2020-01-19 20:10:51 +0800

    add some comment :sparkles:

--- algorithm/sort/sort.go
+++ algorithm/sort/sort.go
@@ -30,17 +30,17 @@ func BubbleAscSort(arr []int) {
 	}
 
 	for i := 0; i < len(arr); i++ {
-		flag := false
+		flag := false // 提前退出冒泡排序的标志
 		for j := 0; j < len(arr)-i-1; j++ {
 			if arr[j] > arr[j+1] {
 				tmp := arr[j+1]
 				arr[j+1] = arr[j]
 				arr[j] = tmp
-				flag = true
+				flag = true // 表示没有进行数据交换
 			}
 		}
 		if !flag {
-			break
+			break // 没有数据交换，提前退出
 		}
 	}
 }
