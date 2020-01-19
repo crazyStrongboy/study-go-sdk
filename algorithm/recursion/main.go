97ed036009abde29739502877d0d169db6d77f20
hejun <13237124836@163.com>
2020-01-19 16:51:32 +0800

    add recursion source :sparkles:

---
+++ algorithm/recursion/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+/*
+@Time : 2020/1/19
+@Author : hejun
+*/
+
+var c map[int]int
+
+func init() {
+	c = map[int]int{}
+}
+
+func main() {
+	fmt.Println(f(5))
+}
+
+func f(n int) int {
+	if n == 1 {
+		return 1
+	}
+	if n == 2 {
+		return 2
+	}
+	if r, ok := c[n]; ok {
+		return r
+	}
+	ret := f(n-1) + f(n-2)
+	c[n] = ret
+	return ret
+}
