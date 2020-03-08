d8fba95065cee27935fbf63165799ac990035480
hejun <13237124836@163.com>
2020-03-08 11:37:45 +0800

    treeNode maxDepth :sparkles:

---
+++ algorithm/leetcode/104/main.go
@@ -0,0 +1,48 @@
+package main
+
+/*
+@Time : 2020/3/8
+@Author : hejun
+*/
+
+/**
+给定一个二叉树，找出其最大深度。
+
+二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+
+说明: 叶子节点是指没有子节点的节点。
+
+示例：
+给定二叉树 [3,9,20,null,null,15,7]，
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回它的最大深度 3 。
+*/
+
+func main() {
+
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return Max(maxDepth(root.Left), maxDepth(root.Right)) + 1
+}
+
+func Max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
