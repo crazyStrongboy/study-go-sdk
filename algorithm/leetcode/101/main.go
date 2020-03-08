3fa2aa55e38be551e89951c359417dd866c664dc
hejun <13237124836@163.com>
2020-03-08 11:21:41 +0800

    isSymmetric :sparkles:

---
+++ algorithm/leetcode/101/main.go
@@ -0,0 +1,55 @@
+package main
+
+/*
+@Time : 2020/3/8
+@Author : hejun
+*/
+
+/**
+给定一个二叉树，检查它是否是镜像对称的。
+
+例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+
+    1
+   / \
+  2   2
+   \   \
+   3    3
+*/
+
+func main() {
+
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSymmetric(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	return isEqual(root.Left, root.Right)
+}
+
+func isEqual(left, right *TreeNode) bool {
+	if left == nil && right == nil {
+		return true
+	}
+	if left == nil || right == nil {
+		return false
+	}
+	if left.Val == right.Val {
+		return isEqual(left.Left, right.Right) && isEqual(left.Right, right.Left)
+	}
+	return false
+}
