b2fd6cb5772988a3f956cd93060e5733cc3a22ea
hejun <13237124836@163.com>
2020-03-17 23:59:52 +0800

    singleNumber :sparkles:

---
+++ algorithm/leetcode/136/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+/*
+@Time : 2020/3/17 23:53
+@Author : hejun
+*/
+
+/**
+给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+
+说明：
+
+你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+
+示例 1:
+
+输入: [2,2,1]
+输出: 1
+示例 2:
+
+输入: [4,1,2,1,2]
+输出: 4
+*/
+
+func main() {
+	fmt.Println(singleNumber([]int{4, 1, 2, 1, 2}))
+}
+
+func singleNumber(nums []int) int {
+loop:
+	for i := 0; i < len(nums); i++ {
+		for j := 0; j < len(nums); j++ {
+			if i != j && nums[j] == nums[i] {
+				continue loop
+			}
+		}
+		return nums[i]
+	}
+	return 0
+}
