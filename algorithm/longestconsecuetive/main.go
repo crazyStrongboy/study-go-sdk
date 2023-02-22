0a9a2ff5a1e0c3c976e18f317928f0399358d695
mars <13237124836@163.com>
2023-02-22 23:34:59 +0800

    longestConsecuetive

--- algorithm/longestconsecuetive/main.go
+++ algorithm/longestconsecuetive/main.go
@@ -1,9 +1,27 @@
 package main
 
 func main(){
-
+	
 }
 
 func longestConsecuetive(nums []int) int {
-	return 0
+	m:=make(map[int]bool, 0)
+	for i := 0; i < len(nums); i++ {
+		m[nums[i]] = true
+	}
+	result:=0
+	for v:=range m {
+		if !m[v-1] {
+			curr:=1
+			temp:=v
+			for m[temp+1]{
+				curr++
+				temp++
+			}
+			if result<curr {
+				result = curr
+			}
+		}
+	}
+	return result
 }
