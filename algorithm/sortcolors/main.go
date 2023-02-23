75992c20234e5198efa39aa73c70b62c35ef72cc
mars <13237124836@163.com>
2023-02-23 23:35:06 +0800

    sortcolors2

--- algorithm/sortcolors/main.go
+++ algorithm/sortcolors/main.go
@@ -9,6 +9,28 @@ func main() {
 }
 
 func sortColors(nums []int) {
+	var j, k, y int
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == 0 {
+			nums[y] = 2
+			nums[k] = 1
+			nums[j] = 0
+			j++
+			k++
+			y++
+		} else if nums[i] == 1 {
+			nums[y] = 2
+			nums[k] = 1
+			k++
+			y++
+		} else {
+			nums[y] = 2
+			y++
+		}
+	}
+}
+
+func sortColors1(nums []int) {
 	for i := 0; i < len(nums); i++ {
 		for j := i + 1; j < len(nums); j++ {
 			if !big(nums[i], nums[j]) {
