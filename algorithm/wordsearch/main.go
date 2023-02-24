40553a26cf8549b53717c308a7035db9bf28ae18
marsjhe <marsjhe@tencent.com>
2023-02-25 00:15:53 +0800

    wordsearch

--- algorithm/wordsearch/main.go
+++ algorithm/wordsearch/main.go
@@ -3,29 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	exist := [][]byte{
+	x := [][]byte{
 		{'A', 'B', 'C', 'E'},
 		{'S', 'F', 'C', 'S'},
 		{'A', 'D', 'E', 'E'},
 	}
 
-	fmt.Println(exist, "ABCCED")
+	fmt.Println(exist(x, "ABCB"))
 }
 
 func exist(board [][]byte, word string) bool {
 	t := &t{
-		used: make(map[int]int),
+		m: len(board),
+		n: len(board[0]),
 	}
-	for i := 0; i < len(board); i++ {
-		t.used[i] = -1
-	}
-	w := []byte(word)
 	for i := 0; i < len(board); i++ {
 		for j := 0; j < len(board[i]); j++ {
-			if board[i][j] == w[0] {
-				if t.backtrack(board, word, 1, i, j) {
-					return true
-				}
+			if t.backtrack(board, word, 0, i, j) {
+				return true
 			}
 		}
 	}
@@ -33,47 +28,26 @@ func exist(board [][]byte, word string) bool {
 }
 
 type t struct {
-	used map[int]int
+	used map[string]int
+	m, n int
 }
 
 func (t *t) backtrack(board [][]byte, word string, index, x, y int) bool {
-	if t.used[x] == y {
+	w := []byte(word)
+	if x < 0 || x > t.m-1 || y < 0 || y > t.n-1 || board[x][y] != w[index] || board[x][y] == '.' {
 		return false
 	}
-	t.used[x] = y
-	defer func() {
-		t.used[x] = -1
-	}()
-	m := len(board)
-	n := len(board[0])
-	w := []byte(word)
-	if len(w)-1 != index {
-		if x-1 >= 0 && y-1 >= 0 && board[x-1][y-1] == w[index] {
-			return t.backtrack(board, word, index+1, x-1, y-1)
-		}
-		if x-1 >= 0 && y < n && board[x-1][y] == w[index] {
-			return t.backtrack(board, word, index+1, x-1, y)
-		}
-		if x+1 < m && y+1 < n && board[x+1][y+1] == w[index] {
-			return t.backtrack(board, word, index+1, x+1, y+1)
-		}
-		if x < m && y+1 < n && board[x][y+1] == w[index] {
-			return t.backtrack(board, word, index+1, x, y+1)
-		}
+	if len(w)-1 == index {
+		return true
 	} else {
-		if x-1 >= 0 && y-1 >= 0 && board[x-1][y-1] == w[index] {
-			return true
-		}
-		if x-1 >= 0 && y < n && board[x-1][y] == w[index] {
-			return true
-		}
-		if x+1 < m && y+1 < n && board[x+1][y+1] == w[index] {
-			return true
-		}
-		if x < m && y+1 < n && board[x][y+1] == w[index] {
-			return true
-		}
-	}
+		temp := board[x][y]
+		board[x][y] = '.'
+		b := t.backtrack(board, word, index+1, x, y-1) ||
+			t.backtrack(board, word, index+1, x-1, y) ||
+			t.backtrack(board, word, index+1, x+1, y) ||
+			t.backtrack(board, word, index+1, x, y+1)
+		board[x][y] = temp
+		return b
 
-	return false
+	}
 }
