d06d59afa3237fe2fa5eb220c33f14f0dbe38b55
marsjhe <marsjhe@tencent.com>
2023-02-26 22:46:19 +0800

    x新增回溯解决方案

--- algorithm/word-break/main.go
+++ algorithm/word-break/main.go
@@ -3,9 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(wordBreak("aaaaaaa", []string{"aaa", "aaaa"}))
+	//fmt.Println("leetcode"[0:4])
+	fmt.Println(wordBreak2("leetcode", []string{"leet", "code"}))
 }
 
+// wordBreak 动态规划
 func wordBreak(s string, wordDict []string) bool {
 	m := make(map[string]bool)
 	for _, w := range wordDict {
@@ -23,4 +25,32 @@ func wordBreak(s string, wordDict []string) bool {
 	}
 	fmt.Println(dp)
 	return dp[len(s)]
+}
+
+// wordBreak2 递归
+func wordBreak2(s string, wordDict []string) bool {
+	m := make(map[string]bool)
+	for _, w := range wordDict {
+		m[w] = true
+	}
+	t := &T{
+		m: m,
+	}
+	return t.backtrack(s, 0)
+}
+
+type T struct {
+	m map[string]bool
+}
+
+func (t *T) backtrack(s string, start int) bool {
+	if start >= len(s) {
+		return true
+	}
+	for i := start; i <= len(s); i++ {
+		if t.m[s[start:i+1]] && t.backtrack(s, i+1) {
+			return true
+		}
+	}
+	return false
 }
