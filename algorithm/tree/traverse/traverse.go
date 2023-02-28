6b39da4e9570801a939ba42eb9a84b9f707fea22
marsjhe <marsjhe@tencent.com>
2023-02-28 19:00:44 +0800

    迭代前中后序遍历tree

--- algorithm/tree/traverse/traverse.go
+++ algorithm/tree/traverse/traverse.go
@@ -2,6 +2,7 @@ package traverse
 
 import (
 	"fmt"
+
 	"github.com/crazyStrongboy/study-go-sdk/algorithm/tree"
 )
 
@@ -9,13 +10,14 @@ import (
 @Time : 2020/2/4
 @Author : hejun
 */
+var result []interface{}
 
 // 前序，先打印任意节点
 func preTraverse(root *tree.Node) {
 	if root == nil {
 		return
 	}
-	fmt.Println(root)
+	result = append(result, root.Elem)
 	preTraverse(root.Left)
 	preTraverse(root.Right)
 }
@@ -26,7 +28,7 @@ func midTraverse(root *tree.Node) {
 		return
 	}
 	midTraverse(root.Left)
-	fmt.Println(root)
+	result = append(result, root.Elem)
 	midTraverse(root.Right)
 }
 
@@ -37,5 +39,96 @@ func postTraverse(root *tree.Node) {
 	}
 	postTraverse(root.Left)
 	postTraverse(root.Right)
-	fmt.Println(root)
+	result = append(result, root.Elem)
+}
+
+func iterMid(root *tree.Node) []interface{} {
+	var (
+		stack []*tree.Node
+		r     []interface{}
+	)
+	stack = append(stack, root)
+	for len(stack) != 0 {
+		elem := stack[len(stack)-1]
+		if elem != nil {
+			stack = stack[:len(stack)-1]
+			if elem.Right != nil {
+				stack = append(stack, elem.Right)
+			}
+			stack = append(stack, elem)
+			stack = append(stack, nil)
+			if elem.Left != nil {
+				stack = append(stack, elem.Left)
+			}
+			continue
+		}
+		elem = stack[len(stack)-2]
+		stack = stack[:len(stack)-2]
+		r = append(r, elem.Elem)
+	}
+	return r
+}
+
+func iterPre(root *tree.Node) []interface{} {
+	var (
+		stack []*tree.Node
+		r     []interface{}
+	)
+	stack = append(stack, root)
+	for len(stack) != 0 {
+		elem := stack[len(stack)-1]
+		if elem != nil {
+			stack = stack[:len(stack)-1]
+			if elem.Right != nil {
+				stack = append(stack, elem.Right)
+			}
+			if elem.Left != nil {
+				stack = append(stack, elem.Left)
+			}
+			stack = append(stack, elem)
+			stack = append(stack, nil)
+			continue
+		}
+		elem = stack[len(stack)-2]
+		stack = stack[:len(stack)-2]
+		r = append(r, elem.Elem)
+	}
+	return r
+}
+
+func iterPost(root *tree.Node) []interface{} {
+	var (
+		stack []*tree.Node
+		r     []interface{}
+	)
+	stack = append(stack, root)
+	for len(stack) != 0 {
+		elem := stack[len(stack)-1]
+		if elem != nil {
+			stack = stack[:len(stack)-1]
+
+			if elem.Left != nil {
+				stack = append(stack, elem.Left)
+			}
+			if elem.Right != nil {
+				stack = append(stack, elem.Right)
+			}
+			stack = append(stack, elem)
+			stack = append(stack, nil)
+
+			continue
+		}
+		elem = stack[len(stack)-2]
+		stack = stack[:len(stack)-2]
+		r = append(r, elem.Elem)
+	}
+	fmt.Println(r)
+	i := 0
+	j := len(r) - 1
+	for i < j {
+		r[i], r[j] = r[j], r[i]
+		i++
+		j--
+	}
+	return r
 }
--- algorithm/tree/traverse/traverse_test.go
+++ algorithm/tree/traverse/traverse_test.go
@@ -1,8 +1,10 @@
 package traverse
 
 import (
-	"github.com/crazyStrongboy/study-go-sdk/algorithm/tree"
+	"fmt"
 	"testing"
+
+	"github.com/crazyStrongboy/study-go-sdk/algorithm/tree"
 )
 
 func Test_preTraverse(t *testing.T) {
@@ -27,4 +29,34 @@ func Test_postTraverse(t *testing.T) {
 	root.Right = tree.NewNode(3)
 	root.Right.Right = tree.NewNode(4)
 	postTraverse(root)
+}
+
+func Test_iterMid(t *testing.T) {
+	root := tree.NewNode(2)
+	root.Left = tree.NewNode(1)
+	root.Right = tree.NewNode(3)
+	root.Right.Right = tree.NewNode(4)
+	midTraverse(root)
+	fmt.Println(result)
+	fmt.Println(iterMid(root))
+}
+
+func Test_iterPre(t *testing.T) {
+	root := tree.NewNode(2)
+	root.Left = tree.NewNode(1)
+	root.Right = tree.NewNode(3)
+	root.Right.Right = tree.NewNode(4)
+	preTraverse(root)
+	fmt.Println(result)
+	fmt.Println(iterPre(root))
+}
+
+func Test_iterPost(t *testing.T) {
+	root := tree.NewNode(2)
+	root.Left = tree.NewNode(1)
+	root.Right = tree.NewNode(3)
+	root.Right.Right = tree.NewNode(4)
+	postTraverse(root)
+	fmt.Println(result)
+	fmt.Println(iterPost(root))
 }
