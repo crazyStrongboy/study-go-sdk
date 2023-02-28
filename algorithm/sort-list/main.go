e7efeb56c94b96767d35a2d924f508086f13b8fd
marsjhe <marsjhe@tencent.com>
2023-02-28 20:19:12 +0800

    sortList第一版

--- algorithm/sort-list/main.go
+++ algorithm/sort-list/main.go
@@ -5,7 +5,24 @@ func main() {
 }
 
 func sortList(head *ListNode) *ListNode {
+	t := head
+	for t != nil {
+		sortSingle(head)
+		t = t.Next
+	}
+	return head
+}
 
+func sortSingle(head *ListNode) {
+	min := head
+	t := head
+	for t != nil {
+		t = t.Next
+		if min.Val > t.Val {
+			min = t
+		}
+	}
+	head.Val, min.Val = min.Val, head.Val
 }
 
 type ListNode struct {
