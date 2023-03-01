0d7081027891f0c68bb632d12f6372364c491d9f
mars <13237124836@163.com>
2023-03-01 11:09:15 +0800

    第二版层次遍历

--- algorithm/binarytreelevelordertraversal/main.go
+++ algorithm/binarytreelevelordertraversal/main.go
@@ -1,7 +1,19 @@
 package main
 
 func main() {
-
+	levelOrder2(&TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val:   2,
+			Left:  nil,
+			Right: nil,
+		},
+		Right: &TreeNode{
+			Val:   3,
+			Left:  &TreeNode{Val: 4},
+			Right: &TreeNode{Val: 5},
+		},
+	})
 }
 
 func levelOrder(root *TreeNode) [][]int {
@@ -20,6 +32,35 @@ func (t *t) recursion(root *TreeNode, depth int) {
 	t.result[depth-1] = append(t.result[depth-1], root.Val)
 	t.recursion(root.Left, depth+1)
 	t.recursion(root.Right, depth+1)
+}
+
+func levelOrder2(root *TreeNode) [][]int {
+	if root == nil {
+		return nil
+	}
+	var (
+		queue  []*TreeNode
+		result [][]int
+	)
+	queue = append(queue, root)
+	for len(queue) > 0 {
+		layer := make([]int, 0, len(queue))
+		size := len(queue)
+		for i := 0; i < size; i++ {
+			temp := queue[0]
+			queue = queue[1:]
+			layer = append(layer, temp.Val)
+			if temp.Left != nil {
+				queue = append(queue, temp.Left)
+			}
+			if temp.Right != nil {
+				queue = append(queue, temp.Right)
+			}
+		}
+		result = append(result, layer)
+	}
+
+	return result
 }
 
 type t struct {
