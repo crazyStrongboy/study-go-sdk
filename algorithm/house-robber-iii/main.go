3583ef7c7582c0500ea9b3ea2e98de2f9be79ae2
mars <13237124836@163.com>
2023-03-03 23:33:37 +0800

    打家劫舍3 第二版

--- algorithm/house-robber-iii/main.go
+++ algorithm/house-robber-iii/main.go
@@ -1,7 +1,25 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	t := &TreeNode{
+		Val: 3,
+		Left: &TreeNode{
+			Val: 2,
+			Right: &TreeNode{
+				Val: 3,
+			},
+		},
+		Right: &TreeNode{
+			Val: 3,
+			Right: &TreeNode{
+				Val: 1,
+			},
+		},
+	}
+
+	fmt.Println(rob(t))
 }
 
 type TreeNode struct {
@@ -11,45 +29,21 @@ type TreeNode struct {
 }
 
 func rob(root *TreeNode) int {
-	var (
-		result []int
-		stack  []*TreeNode
-	)
+	result := traverse(root)
+	return max(result[0], result[1])
+}
+
+// 数组 [0]=表示打劫自己能获取的最大值  [1]=表示打劫子节点能获取的最大值
+func traverse(root *TreeNode) []int {
 	if root == nil {
-		return 0
+		return []int{0, 0}
 	}
-	stack = append(stack, root)
-	// 先进行层次遍历求和数组
-	for len(stack) > 0 {
-		size := len(stack)
-		sum := 0
-		for i := 1; i <= size; i++ {
-			t := stack[0]
-			sum += t.Val
-			stack = stack[1:]
-			if t.Left != nil {
-				stack = append(stack, t.Left)
-			}
-			if t.Right != nil {
-				stack = append(stack, t.Right)
-			}
-		}
-		result = append(result, sum)
-	}
-	// 开始打劫
-	if len(result) == 1 {
-		return result[0]
-	}
-	if len(result) == 2 {
-		return max(result[0], result[1])
-	}
-	dp := make([]int, len(result))
-	dp[0] = result[0]
-	dp[1] = max(result[0], result[1])
-	for i := 2; i < len(result); i++ {
-		dp[i] = max(dp[i-2]+result[i], dp[i-1])
-	}
-	return dp[len(result)-1]
+	l := traverse(root.Left)
+	r := traverse(root.Right)
+
+	ret := []int{root.Val + l[1] + r[1], max(l[0], l[1]) + max(r[0], r[1])}
+	fmt.Println(ret)
+	return ret
 }
 
 func max(a, b int) int {
