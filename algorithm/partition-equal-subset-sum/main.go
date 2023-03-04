a29fe6cfa157089982d13d86d99759a7e622ad1e
mars <13237124836@163.com>
2023-03-05 00:53:56 +0800

    分隔等和子集第二版动态规划

--- algorithm/partition-equal-subset-sum/main.go
+++ algorithm/partition-equal-subset-sum/main.go
@@ -1,7 +1,9 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Println(canPartition([]int{14, 9, 8, 4, 3, 2}))
 }
 
 func canPartition(nums []int) bool {
@@ -9,13 +11,38 @@ func canPartition(nums []int) bool {
 	for i := 0; i < len(nums); i++ {
 		sum += nums[i]
 	}
+	fmt.Println(sum)
 	if sum%2 != 0 {
 		return false
 	}
-	return backtrack(nums, 0, sum/2)
+	return bag(nums, sum/2)
+}
+
+func bag(nums []int, weight int) bool {
+	dp := make([]int, weight+1)
+	dp[0] = 0
+	for i := 0; i < len(nums); i++ {
+		for j := weight; j > 0; j-- {
+			if j >= nums[i] {
+				dp[j] = max(dp[j-nums[i]]+nums[i], dp[j])
+			}
+		}
+		//fmt.Println(dp)
+	}
+	return dp[weight] == weight
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
 
 func backtrack(nums []int, start, target int) bool {
+	if target < 0 {
+		return false
+	}
 	if target == 0 {
 		return true
 	}
