914d3614f8214adddfb014aaf20d3dd8a5f79625
mars <13237124836@163.com>
2023-03-05 11:18:29 +0800

    和为k的子数组 第二版  前缀和

--- algorithm/subarray-sum-equals-k/main.go
+++ algorithm/subarray-sum-equals-k/main.go
@@ -8,7 +8,7 @@ func main() {
 	fmt.Println(subarraySum([]int{1, 2, 1, 2, 1}, 3))
 }
 
-func subarraySum(nums []int, k int) int {
+func subarraySum1(nums []int, k int) int {
 	count := 0
 	for i := 0; i < len(nums); i++ {
 		sum := 0
@@ -21,6 +21,25 @@ func subarraySum(nums []int, k int) int {
 			}
 			j++
 		}
+	}
+	return count
+}
+
+// nums[i]-nums[j] = k  (0<=j<=i)
+// nums[j] = nums[i] - k
+// 存储所有的前缀和在一个map里面，只要满足map[nums[i] - k]不为空，则表示有对应的index
+func subarraySum(nums []int, k int) int {
+	pre := 0
+	count := 0
+	m := make(map[int]int)
+	m[0] = 1 // 初始化0为1，有可能是从index为0开始的
+	for i := 0; i < len(nums); i++ {
+		pre += nums[i] // 以i结尾的前缀和
+		if v, ok := m[pre-k]; ok {
+			count += v
+		}
+		m[pre] = m[pre] + 1
+		//fmt.Println(m)
 	}
 	return count
 }
