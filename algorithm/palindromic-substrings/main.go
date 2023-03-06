8bb0973c4af58d0a6e5f0534c8beb5a12d8982bf
mars <13237124836@163.com>
2023-03-07 00:57:13 +0800

    回文串第一版

---
+++ algorithm/palindromic-substrings/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(countSubstrings("aaa"))
+}
+
+func countSubstrings(s string) int {
+	sum := 0
+	for i := 0; i < len(s); i++ {
+		sum += count(s, i, i, len(s)-1)
+		sum += count(s, i, i+1, len(s)-1)
+	}
+	return sum
+}
+
+func count(s string, i, j, n int) int {
+	sum := 0
+	for i >= 0 && j <= n && s[i] == s[j] {
+		sum++
+		i--
+		j++
+	}
+	return sum
+}
