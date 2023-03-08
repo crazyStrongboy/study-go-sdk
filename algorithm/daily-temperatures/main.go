3d59a883c45982d23628c169f1561d5e36864bf2
mars <13237124836@163.com>
2023-03-09 01:43:13 +0800

    每日温度  单调栈ac

--- algorithm/daily-temperatures/main.go
+++ algorithm/daily-temperatures/main.go
@@ -3,10 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(dailyTemperatures([]int{30, 40, 50, 60}))
+	fmt.Println(dailyTemperatures([]int{73, 74, 75, 71, 69, 72, 76, 73}))
 }
 
-func dailyTemperatures(temperatures []int) []int {
+func dailyTemperatures1(temperatures []int) []int {
 	result := make([]int, len(temperatures))
 	for i := 0; i < len(temperatures); i++ {
 		j := i + 1
@@ -17,6 +17,39 @@ func dailyTemperatures(temperatures []int) []int {
 			}
 			j++
 		}
+	}
+	return result
+}
+
+// 倒着遍历，如果temperatures[i] < temperatures[j],那么result[i] = j-i
+// 如果temperatures[i] > temperatures[j],并且result[j]==0,那么result[i] =0
+func dailyTemperatures2(temperatures []int) []int {
+	result := make([]int, len(temperatures))
+	for i := len(temperatures) - 2; i >= 0; i-- {
+		for j := i + 1; j < len(temperatures); j++ {
+			if temperatures[i] < temperatures[j] {
+				result[i] = j - i
+				break
+			} else if result[j] == 0 {
+				//result[i] = 0 // 这里可以注释掉不写
+				break
+			}
+		}
+	}
+	return result
+}
+
+// 单调栈
+func dailyTemperatures(temperatures []int) []int {
+	result := make([]int, len(temperatures))
+	var stack []int
+	for i := 0; i < len(temperatures); i++ {
+		for len(stack) > 0 && temperatures[i] > temperatures[stack[len(stack)-1]] {
+			result[stack[len(stack)-1]] = i - stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+		}
+		stack = append(stack, i)
+		fmt.Println(stack)
 	}
 	return result
 }
