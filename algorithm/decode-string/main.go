1a2f3d462342394554b7140d6fa3d268b01c1f76
mars <13237124836@163.com>
2023-03-11 15:06:17 +0800

    字符串解码

---
+++ algorithm/decode-string/main.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(decodeString("100[leetcode]"))
+}
+
+func decodeString(s string) string {
+	result := ""
+	i := 0
+	mul := 0
+	for i < len(s) {
+		if s[i] >= 'a' && s[i] <= 'z' {
+			result += string(s[i])
+		} else if s[i] >= '0' && s[i] <= '9' {
+			if mul == 0 {
+				mul = int(s[i] - '0')
+			} else {
+				mul = mul*10 + int(s[i]-'0')
+			}
+		} else if s[i] == '[' {
+			end := findEnd(s, i)
+			tmp := decodeString(s[i+1 : end])
+			xx := ""
+			for mul >= 1 {
+				xx += tmp
+				mul--
+			}
+			result += xx
+			i = end
+			mul = 0
+		}
+		i++
+	}
+	return result
+}
+
+func findEnd(s string, index int) int {
+	var stack []int
+	stack = append(stack, 1)
+	end := index + 1
+	for index < len(s) && len(stack) > 0 {
+		if s[end] == '[' {
+			stack = append(stack, 1)
+		} else if s[end] == ']' {
+			stack = stack[:len(stack)-1]
+		}
+		end++
+	}
+	return end - 1
+}
