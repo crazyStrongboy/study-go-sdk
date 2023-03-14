cccfb9c2bbe2df2916f0f21762760db212480b8c
mars <13237124836@163.com>
2023-03-15 01:25:52 +0800

    最长有效括号

--- algorithm/_second_brush/02/02.go
+++ algorithm/_second_brush/02/02.go
@@ -162,3 +162,21 @@ func reverse(nums []int) {
 		j--
 	}
 }
+
+func longestValidParentheses(s string) int {
+	stack := []int{-1}
+	result := 0
+	for i := 0; i < len(s); i++ {
+		if s[i] == '(' {
+			stack = append(stack, i)
+		} else {
+			stack = stack[:len(stack)-1]
+			if len(stack) == 0 {
+				stack = append(stack, i)
+			} else {
+				result = max(result, i-stack[len(stack)-1])
+			}
+		}
+	}
+	return result
+}
