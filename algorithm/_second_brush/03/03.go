1313466744540512570862b33859d7757b6080a6
mars <13237124836@163.com>
2023-03-15 21:34:39 +0800

    组合总和

--- algorithm/_second_brush/03/03.go
+++ algorithm/_second_brush/03/03.go
@@ -1,5 +1,7 @@
 package main
 
+import "sort"
+
 func searchRange(nums []int, target int) []int {
 	left := 0
 	right := len(nums) - 1
@@ -29,4 +31,32 @@ func searchRange(nums []int, target int) []int {
 		end++
 	}
 	return []int{start + 1, end - 1}
+}
+
+func combinationSum(candidates []int, target int) [][]int {
+	sort.Ints(candidates)
+	s := &Sum{}
+	s.backtrack(candidates, 0, target, []int{})
+	return s.result
+}
+
+type Sum struct {
+	result [][]int
+}
+
+func (s *Sum) backtrack(candidates []int, start, target int, track []int) {
+	if target == 0 {
+		t := make([]int, len(track))
+		copy(t, track)
+		s.result = append(s.result, t)
+		return
+	}
+	for i := start; i < len(candidates); i++ {
+		if target < candidates[i] {
+			return
+		}
+		track = append(track, candidates[i])
+		s.backtrack(candidates, i, target-candidates[i], track)
+		track = track[:len(track)-1]
+	}
 }
