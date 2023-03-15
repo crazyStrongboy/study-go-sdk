9cc7319599b3e637d74526b2780c15c6185be839
mars <13237124836@163.com>
2023-03-16 00:07:36 +0800

    爬楼梯  dp

--- algorithm/_second_brush/03/03.go
+++ algorithm/_second_brush/03/03.go
@@ -241,3 +241,16 @@ func minPathSum(grid [][]int) int {
 	}
 	return dp[len(grid)-1][len(grid[0])-1]
 }
+
+func climbStairs(n int) int {
+	if n <= 1 {
+		return n
+	}
+	dp := make([]int, n+1)
+	dp[1] = 1
+	dp[2] = 2
+	for i := 3; i <= n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+	return dp[n]
+}
