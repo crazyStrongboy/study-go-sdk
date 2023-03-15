e32bc69cddcff9d64c82e01613dd3755a84fa71b
mars <13237124836@163.com>
2023-03-16 01:12:38 +0800

    颜色分类 双指针

--- algorithm/_second_brush/03/03.go
+++ algorithm/_second_brush/03/03.go
@@ -339,3 +339,20 @@ func sortColors1(nums []int) {
 		}
 	}
 }
+
+func sortColors2(nums []int) {
+	p0, p1 := 0, 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == 0 {
+			nums[i], nums[p0] = nums[p0], nums[i]
+			if nums[p1] > nums[i] {
+				nums[i], nums[p1] = nums[p1], nums[i]
+			}
+			p0++
+			p1++
+		} else if nums[i] == 1 {
+			nums[i], nums[p1] = nums[p1], nums[i]
+			p1++
+		}
+	}
+}
