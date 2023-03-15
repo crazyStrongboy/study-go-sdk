9cbd40a577676cb88f1b005610a11703c8462251
mars <13237124836@163.com>
2023-03-16 00:02:56 +0800

    最小路径和  dp

--- algorithm/_second_brush/03/03.go
+++ algorithm/_second_brush/03/03.go
@@ -220,3 +220,24 @@ func uniquePaths(m int, n int) int {
 	}
 	return dp[m-1][n-1]
 }
+
+func minPathSum(grid [][]int) int {
+	var dp [][]int
+	for i := 0; i < len(grid); i++ {
+		dp = append(dp, make([]int, len(grid[0])))
+	}
+	dp[0][0] = grid[0][0]
+	for i := 1; i < len(grid); i++ {
+		dp[i][0] = dp[i-1][0] + grid[i][0]
+	}
+	for j := 1; j < len(grid[0]); j++ {
+		dp[0][j] = dp[0][j-1] + grid[0][j]
+	}
+
+	for i := 1; i < len(grid); i++ {
+		for j := 1; j < len(grid[0]); j++ {
+			dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
+		}
+	}
+	return dp[len(grid)-1][len(grid[0])-1]
+}
