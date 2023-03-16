adddb288ed4347a90064da48e0316cb27c67db1e
mars <13237124836@163.com>
2023-03-16 23:42:46 +0800

    单词搜索

--- algorithm/_second_brush/04/04.go
+++ algorithm/_second_brush/04/04.go
@@ -26,3 +26,33 @@ func (s *subSet) backtrack(nums []int, start int, track []int) {
 		// s.used[nums[i]] = false
 	}
 }
+
+func exist(board [][]byte, word string) bool {
+	for i := 0; i < len(board); i++ {
+		for j := 0; j < len(board[0]); j++ {
+			if backtrack(board, word, i, j, 0) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func backtrack(board [][]byte, word string, i, j, index int) bool {
+	if i < 0 || j < 0 || i > len(board)-1 || j > len(board[0])-1 || board[i][j] == '.' || board[i][j] != word[index] {
+		return false
+	}
+	if index == len(word)-1 {
+		return true
+	} else {
+		temp := board[i][j]
+		board[i][j] = '.'
+		if backtrack(board, word, i, j-1, index+1) || backtrack(board, word, i, j+1, index+1) ||
+			backtrack(board, word, i-1, j, index+1) || backtrack(board, word, i+1, j, index+1) {
+			return true
+		}
+		board[i][j] = temp
+	}
+	return false
+
+}
