53513d912a4cba3879593565481de66a6b3450f9
mars <13237124836@163.com>
2023-03-17 01:21:16 +0800

    二叉树最大深度

--- algorithm/_second_brush/04/04.go
+++ algorithm/_second_brush/04/04.go
@@ -209,3 +209,10 @@ func levelOrder(root *TreeNode) [][]int {
 	}
 	return result
 }
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return max(maxDepth(root.Left)+1, maxDepth(root.Right)+1)
+}
