5fd7c355824b0ecff8c6cd9fed0255376aa790a5
mars <13237124836@163.com>
2023-03-18 14:54:10 +0800

    打家劫舍

--- algorithm/_second_brush/06/06.go
+++ algorithm/_second_brush/06/06.go
@@ -87,3 +87,16 @@ func majorityElement(nums []int) int {
 	}
 	return 0
 }
+
+func rob(nums []int) int {
+	if len(nums) < 2 {
+		return nums[0]
+	}
+	dp := make([]int, len(nums)+1)
+	dp[1] = nums[0]
+	dp[2] = max(nums[0], nums[1])
+	for i := 2; i <= len(nums); i++ {
+		dp[i] = max(dp[i-2]+nums[i-1], dp[i-1])
+	}
+	return dp[len(nums)]
+}
