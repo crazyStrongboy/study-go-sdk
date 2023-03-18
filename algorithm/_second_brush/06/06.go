8ff6200f7354d5951f7f1b2a9d05ca0f5c2c611d
mars <13237124836@163.com>
2023-03-18 14:40:06 +0800

    多数元素

--- algorithm/_second_brush/06/06.go
+++ algorithm/_second_brush/06/06.go
@@ -74,3 +74,16 @@ func getListNodeLen(head *ListNode) int {
 	}
 	return cnt
 }
+
+func majorityElement(nums []int) int {
+	m := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		m[nums[i]]++
+	}
+	for k, v := range m {
+		if v > len(nums)/2 {
+			return k
+		}
+	}
+	return 0
+}
