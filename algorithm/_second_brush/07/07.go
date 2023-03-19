ec201aeeb6dd19e802d37f63e38c66cdb962ccf2
mars <13237124836@163.com>
2023-03-19 21:43:04 +0800

    零钱兑换

--- algorithm/_second_brush/07/07.go
+++ algorithm/_second_brush/07/07.go
@@ -352,3 +352,22 @@ func maxCoins(nums []int) int {
 	}
 	return dp[0][len(nums)-1]
 }
+
+func coinChange(coins []int, amount int) int {
+	dp := make([]int, amount+1)
+	for i := 1; i <= amount; i++ {
+		dp[i] = 10001
+	}
+	for i := 1; i <= amount; i++ {
+		for j := 0; j < len(coins); j++ {
+			if i >= coins[j] {
+				dp[i] = min(dp[i], dp[i-coins[j]]+1)
+			}
+		}
+		//fmt.Println(dp)
+	}
+	if dp[amount] == 10001 {
+		return -1
+	}
+	return dp[amount]
+}
