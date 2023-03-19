b2132436379e35817a73d979668a980356ca022e
mars <13237124836@163.com>
2023-03-19 15:30:26 +0800

    搜索二维矩阵2

--- algorithm/_second_brush/07/07.go
+++ algorithm/_second_brush/07/07.go
@@ -104,3 +104,18 @@ func maxSlidingWindow(nums []int, k int) []int {
 	}
 	return result
 }
+
+func searchMatrix(matrix [][]int, target int) bool {
+	i := len(matrix) - 1
+	j := 0
+	for i >= 0 && j < len(matrix[0]) {
+		if matrix[i][j] == target {
+			return true
+		} else if matrix[i][j] < target {
+			j++
+		} else {
+			i--
+		}
+	}
+	return false
+}
