877dcfe553592623fa62e75d0d6056d6329fa6e4
mars <13237124836@163.com>
2023-03-19 20:53:34 +0800

    最近买卖股票含冷冻期

--- algorithm/_second_brush/07/07.go
+++ algorithm/_second_brush/07/07.go
@@ -317,3 +317,20 @@ func isValid(s string) bool {
 	}
 	return cnt == 0
 }
+
+func maxProfit(prices []int) int {
+	dp := make([][]int, len(prices))
+	for i := 0; i < len(prices); i++ {
+		dp[i] = make([]int, 3)
+		dp[0][0] = -prices[0]
+	}
+
+	for i := 1; i < len(prices); i++ {
+		dp[i][0] = max(dp[i-1][0], dp[i-1][2]-prices[i])
+		dp[i][1] = max(dp[i-1][0]+prices[i], dp[i-1][1])
+		dp[i][2] = dp[i-1][1]
+	}
+	//fmt.Println(dp)
+
+	return max(dp[len(prices)-1][1], dp[len(prices)-1][2]) // 卖出状态和冷冻期取最大值
+}
