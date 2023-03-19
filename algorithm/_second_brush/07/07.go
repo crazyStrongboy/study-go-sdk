5edd604fc999b3536843798b58b30cadfa830c8e
mars <13237124836@163.com>
2023-03-19 16:35:41 +0800

    寻找重复数

--- algorithm/_second_brush/07/07.go
+++ algorithm/_second_brush/07/07.go
@@ -152,3 +152,21 @@ func moveZeroes(nums []int) {
 		nums[i] = 0
 	}
 }
+
+func findDuplicate(nums []int) int {
+	slow := 0
+	fast := 0
+	for {
+		slow = nums[slow]
+		fast = nums[nums[fast]]
+		if slow == fast {
+			fast = 0
+			for nums[slow] != nums[fast] {
+				slow = nums[slow]
+				fast = nums[fast]
+			}
+			return nums[slow]
+		}
+	}
+	return -1
+}
