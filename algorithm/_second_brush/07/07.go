d53cac14703b94fe4ba81b64045f88719a98358c
mars <13237124836@163.com>
2023-03-19 15:02:08 +0800

    二叉树最近公共祖先

--- algorithm/_second_brush/07/07.go
+++ algorithm/_second_brush/07/07.go
@@ -51,3 +51,18 @@ func isPalindrome(head *ListNode) bool {
 
 	return true
 }
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil || root == p || root == q {
+		return root
+	}
+	l := lowestCommonAncestor(root.Left, p, q)
+	r := lowestCommonAncestor(root.Right, p, q)
+	if l != nil && r != nil {
+		return root
+	}
+	if l == nil {
+		return r
+	}
+	return l
+}
