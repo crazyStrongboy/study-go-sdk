4a51b5bfe13ba23a22801593a4982ed64ca8d01b
mars <13237124836@163.com>
2023-03-20 22:49:53 +0800

    前k个高频元素

--- algorithm/_second_brush/08/08.go
+++ algorithm/_second_brush/08/08.go
@@ -1,9 +1,32 @@
 package main
 
+import "sort"
+
 func countBits(n int) []int {
 	bits := make([]int, n+1)
 	for i := 1; i <= n; i++ {
 		bits[i] = bits[i&(i-1)] + 1
 	}
 	return bits
+}
+
+func topKFrequent(nums []int, k int) []int {
+	m := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		m[nums[i]]++
+	}
+	var result [][]int
+	for k, v := range m {
+		result = append(result, []int{k, v})
+	}
+
+	sort.Slice(result, func(i, j int) bool {
+		return result[i][1] > result[j][1]
+	})
+
+	ret := make([]int, k)
+	for i := 0; i < k; i++ {
+		ret[i] = result[i][0]
+	}
+	return ret
 }
