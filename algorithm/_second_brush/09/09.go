d37a45436783fdc6302ff078c97f7871cef0fce5
mars <13237124836@163.com>
2023-03-21 22:43:00 +0800

    目标和

--- algorithm/_second_brush/09/09.go
+++ algorithm/_second_brush/09/09.go
@@ -1,5 +1,7 @@
 package main
 
+import "math"
+
 func findDisappearedNumbers(nums []int) []int {
 	n := len(nums)
 	for i := 0; i < len(nums); i++ {
@@ -24,4 +26,31 @@ func hammingDistance(x int, y int) int {
 		x &= x - 1
 	}
 	return count
+}
+
+func findTargetSumWays(nums []int, target int) int {
+	sum := 0
+	for i := 0; i < len(nums); i++ {
+		sum += nums[i]
+	}
+	if sum < int(math.Abs(float64(target))) {
+		return 0
+	}
+	sum += target
+	if sum < 0 {
+		sum = -sum
+	}
+	if sum%2 != 0 {
+		return 0
+	}
+	dp := make([]int, sum/2+1)
+	dp[0] = 1
+	for i := 0; i < len(nums); i++ {
+		for j := sum / 2; j >= 0; j-- {
+			if j >= nums[i] {
+				dp[j] += dp[j-nums[i]]
+			}
+		}
+	}
+	return dp[sum/2]
 }
