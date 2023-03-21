465ab9c9308a34ae389407663899d5b21a8812fd
mars <13237124836@163.com>
2023-03-21 23:11:16 +0800

    和为k的子数组

--- algorithm/_second_brush/09/09.go
+++ algorithm/_second_brush/09/09.go
@@ -88,3 +88,17 @@ func depth(root *TreeNode) int {
 	}
 	return max(depth(root.Left), depth(root.Right)) + 1
 }
+
+func subarraySum(nums []int, k int) int {
+	count := 0
+	for i := 0; i < len(nums); i++ {
+		sum := 0
+		for j := i; j < len(nums); j++ {
+			sum += nums[j]
+			if sum == k {
+				count++
+			}
+		}
+	}
+	return count
+}
